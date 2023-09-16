10811fe1758d4b6ab0df2acca837ac80125bd66a
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-09-16 11:55:27 +0900

    네비게이션바 디자인

--- updown/src/App.js
+++ updown/src/App.js
@@ -9,7 +9,7 @@ export default function start(){
            <nav className="nav">
                <ul>
                    <li><a herf="#">홈</a></li>
-                   <li><a herf="UPandDownMain.js">선배가 후배에게</a></li>
+                   <li id="MainTitle"><a herf="UPandDownMain.js">선배가 후배에게</a></li>
                    <li><a herf="#">포트폴리오</a></li>
                    <li><a herf="#">내정보</a></li>
                </ul>
