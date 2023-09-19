8874026975cfea66aec582cc53ec45eb150abde2
박규민 <131234408+kyumin7487@users.noreply.github.com>
2023-09-19 11:19:41 +0900

    Sopo

---
+++ updown/src/post.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function start(){
+  return( 
+  <div className="content">
+       <header className="header">
+           <h1><a herf="">SOPO</a></h1>
+       
+           <nav className="nav">
+               <ul>
+                   <li><a herf="#">홈</a></li>
+                   <li id="MainTitle"><a herf="UPandDownMain.js">선배가 후배에게</a></li>
+                   <li><a herf="#">포트폴리오</a></li>
+                   <li><a herf="#">내정보</a></li>
+               </ul>
+           </nav>
+       </header>
+  </div>
+  
+  )
+}
